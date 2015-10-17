6e74c9dd5d4d8be6bec8cea06eee310a78339bbd
LeeSD90 <lee.doyle90@gmail.com>
2015-10-17 20:29:32 +0100

    Added question 2 solution

--- euler_ruby.rb
+++ euler_ruby.rb
@@ -1,14 +1,36 @@
 def multiples_of_3_and_5(number)
 	sum = 0
 	i = 1
-	while(i<number)
+
+	while(i < number)
 		if((i % 3 == 0) || (i % 5 == 0))
-			puts i
 			sum = sum + i
 		end
-		i = i + 1
+		i += 1
 	end
-	puts sum
+
+	puts "The sum of numbers divisible by 3 or 5 is #{sum}"
 end
 
 multiples_of_3_and_5(1000)
+
+def even_fibonacci_sum(max)
+	sum = 0
+	current = 2
+	previous = 1
+	temp = 0
+
+	while(current < max)
+		puts current
+		if (current % 2 == 0) 
+			sum += current
+		end
+		temp = current
+		current += previous
+		previous = temp 
+	end
+
+	puts "The sum of each even term in the fibonacci sequence is #{sum}"
+end
+
+even_fibonacci_sum(4000000)
